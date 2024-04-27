18f6643a2c9dd5b88a92ee83d22c7718c88d634c
akashadas10 <akashdas27904@gmail.com>
2024-04-27 20:49:06 +0530

    First Commit

---
+++ CSS/Portfolio/portfolio.js
@@ -0,0 +1,25 @@
+let a=2;
+let b=3;
+a='Akash';
+let s=a=b;
+console.log(s);
+let c=5;
+c='5';
+console.log(typeof(s));
+
+let year=2024;
+if(year%4==0 && year%100 ==0 && year%400 == 0)
+    console.log("leapyear");
+
+else
+    console.log("not leapyear");
+
+let n=4;
+
+for(let i=0; i<n; i++){
+    let text='';
+    for(let j=0; j<i; j++){
+        text+='* '  
+    }
+}
+
